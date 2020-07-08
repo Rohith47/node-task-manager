809269f7b8047fc8d70414e6cec7b61178db9a0d
Rohith S R <rohith47@gmail.com>
2020-07-08 21:13:04 +0200

    adding new features and env config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,15 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect(process.env.MONGO_DB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
 });
-
-// const task = new Task({
-//     description: 'Its a brand new task      ',
-//     completed: false
-// })
-
-// task.save().then(() => console.log(task)).catch((error) => console.log(error));
--- src/index.js
+++ src/index.js
@@ -4,19 +4,7 @@ const userRouter =  require("./routers/user");
 const taskRouter =  require("./routers/task");
 
 const app = express();
-const port = process.env.PORT || 3002;
-
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled!!')
-//     } else {
-//         next();
-//     }
-// });
-
-// app.use((req, res, next) => {
-//     res.status(503).send()
-// });
+const port = process.env.PORT;
 
 app.use(express.json());
 
@@ -27,16 +15,4 @@ app.listen(port, () => {
     console.log('Server is up on port ', port);
     
 });
-
-
-const multer = require('multer');
-
-const upload = new multer({
-    dest: 'images'
-});
-
-app.post('/upload', upload.single('upload'), (req,res) => {
-    res.send();
-});
-
 
